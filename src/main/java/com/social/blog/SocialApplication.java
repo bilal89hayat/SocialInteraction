7886676a8593a0c64365bcba322d1b155d54205e
Bilal Hayat <bhayat@XEBIAINDIA.LOCAL>
2018-12-17 14:27:01 +0530

    First Commit

---
+++ src/main/java/com/social/blog/SocialApplication.java
@@ -0,0 +1,17 @@
+package com.social.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialApplication.class, args);
+	}
+}
+
